6236ee864d93fe8d126cab60226018cccab0c674
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-12-22 17:52:34 +0500

    создания пути и сохранение все вынесенно по нормальному наверное

--- dev/stages/loadingGame.ts
+++ dev/stages/loadingGame.ts
@@ -4,9 +4,8 @@ interface InterfaceLoadingGame {
     init: ()=> void;
     loadingLoop: ()=> void;
     paintLoading: ()=> void;
-    paintRectangleBorderRadius: ()=> void;
+    CreateRectangleBorderRadius: (left:number, bottom:number, width:number, lineThickness:number)=> Path2D;
 }
-
 
 class LoadingGame implements InterfaceLoadingGame{
     game: Game;
@@ -15,18 +14,24 @@ class LoadingGame implements InterfaceLoadingGame{
         width: number,
         left: number,
         lineThickness: number,
-        strokeWidth: number
+        lineWidth: number
     };
+    paths: {
+        loadingWrapper: Path2D
+    }
 
 
     constructor(game: Game) {
         this.game = game;
+        this.paths = {
+            loadingWrapper: undefined
+        };
         this.loadingDrawParametrs = {
             bottom: game.windowSize.height - game.windowSize.height / 100 * 17,
             width: game.windowSize.width / 100 * 80,
             left: game.windowSize.width / 100 * 10,
             lineThickness: 10,
-            strokeWidth: 2
+            lineWidth: .5
         }
     }
 
@@ -38,7 +43,11 @@ class LoadingGame implements InterfaceLoadingGame{
     }
 
     paintLoading(): void{
-        this.paintRectangleBorderRadius();
+        if (!this.paths.loadingWrapper) {
+            let {left, bottom, width, lineThickness} = this.loadingDrawParametrs;
+            this.paths.loadingWrapper = this.CreateRectangleBorderRadius(left, bottom, width, lineThickness);
+        };
+        this.paintDrawLoadingWrapper();
 
     }
 
@@ -46,26 +55,29 @@ class LoadingGame implements InterfaceLoadingGame{
         requestAnimationFrame(()=>this.loadingLoop());
     }
 
-    paintRectangleBorderRadius(): void {
-        let ctx = this.game.mainContext,
-            { left, bottom, width, lineThickness } = this.loadingDrawParametrs,
-            angleDivision = lineThickness / 2;
-
+    paintDrawLoadingWrapper(): void {
+        let ctx = this.game.mainContext;
         ctx.beginPath();
-
         ctx.fillStyle = '#09082f';
         ctx.strokeStyle = "#fff";
-        ctx.lineWidth = 0.5;
-        ctx.moveTo(left, bottom);
-        ctx.quadraticCurveTo(left, bottom - angleDivision, left + angleDivision, bottom - angleDivision);
-        ctx.lineTo(left + width - angleDivision , bottom - angleDivision);
-        ctx.quadraticCurveTo(left + width, bottom - angleDivision, left + width, bottom);
-        ctx.quadraticCurveTo(left + width, bottom + angleDivision, left + width - angleDivision, bottom + angleDivision);
-        ctx.lineTo(left + angleDivision, bottom + angleDivision);
-        ctx.quadraticCurveTo(left, bottom + angleDivision, left, bottom);
-        ctx.closePath()
-        ctx.fill();
-        ctx.stroke();
+        ctx.lineWidth = this.loadingDrawParametrs.lineWidth;
+        ctx.fill(this.paths.loadingWrapper);
+        ctx.stroke(this.paths.loadingWrapper);
+    }
+
+    CreateRectangleBorderRadius(left:number, bottom:number, width:number, lineThickness:number): Path2D {
+        let path = new Path2D(),
+            angleDivision = lineThickness / 2;
+
+        path.moveTo(left, bottom);
+        path.quadraticCurveTo(left, bottom - angleDivision, left + angleDivision, bottom - angleDivision);
+        path.lineTo(left + width - angleDivision , bottom - angleDivision);
+        path.quadraticCurveTo(left + width, bottom - angleDivision, left + width, bottom);
+        path.quadraticCurveTo(left + width, bottom + angleDivision, left + width - angleDivision, bottom + angleDivision);
+        path.lineTo(left + angleDivision, bottom + angleDivision);
+        path.quadraticCurveTo(left, bottom + angleDivision, left, bottom);
+        path.closePath()
+        return path;
     }
 
 }
