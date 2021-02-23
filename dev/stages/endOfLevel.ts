29db0bfca6f990e4f215beaa181853a75a0b164c
vitaliyeb <vlvlv.vovov@bk.ru>
2021-02-23 16:53:40 +0300

    jgbcsdf. btnjl jnhbcjdrb

--- dev/stages/endOfLevel.ts
+++ dev/stages/endOfLevel.ts
@@ -11,6 +11,8 @@ export default class EndOfLevel implements InterfaceEndOfLevel{
         radius: number;
         x: number;
         y: number;
+        gradient: CanvasGradient;
+        
     }
 
     constructor(game: Game) {
@@ -21,25 +23,38 @@ export default class EndOfLevel implements InterfaceEndOfLevel{
         this.game.setBackground('mainBg', true);
         this.game.clearMainCanvas();
         if(!this.arcData) this.arcSetData();
+        this.paintArc();
 
     }
 
     arcSetData() {
         let {width, height} = this.game.windowSize,
             minSize =   Math.min(width, height),
-            radius = minSize / 100 * 15;
+            radius = minSize / 100 * 15,
+            gradient = this.game.mainContext.createLinearGradient(0, 0, width, height);
+            gradient.addColorStop(0, '#697af3');
+            gradient.addColorStop(1, '#7d42d1');
 
         this.arcData = {
             x: width / 2,
-            y: height / 100 * 10 + radius,
-            radius
+            y: height / 100 * 20 + radius,
+            radius,
+            gradient
         }
     }
 
     paintArc() {
-        let ctx = this.game.mainContext;
+        let ctx = this.game.mainContext,
+            {x, y, radius, gradient} = this.arcData;
 
-
-
+        ctx.beginPath();
+        ctx.fillStyle = gradient;
+        ctx.arc(x, y, radius, 0, Math.PI*2)
+        ctx.fill();
+        ctx.fillStyle = '#fff';
+        ctx.font = `${radius * .28}px Roboto`;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(String(this.game.user.levelCount), x, y);
     }
 }
